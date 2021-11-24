d65cd7912f07da875eedbbf22f7c0001354991d9
关于 <372301467@qq.com>
2021-11-24 15:58:27 +0800

    v0.1.1 增加回调 onInit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const Http = require('http')
 const HttpRouter = require('@ppzp/http-router')
 const Context = require('./context')
-const defaults = require('@ppzp/utils/defaults')
+const { defaults, promiseAll } = require('@ppzp/utils')
 const returnData = require('./breads/return-data')
 
 module.exports = class Resh {
   constructor(options) {
     options = defaults(options, {
-      Context,
+      returnData: true,
       breads: [],
-      returnData: true
+      onInit: [],
+      Context
     })
 
     if(options.returnData)
       options.breads.unshift(returnData)
+    this.__onInit = options.onInit
+
     this.router = new HttpRouter(options)
     if(options.controllers)
       this.router.setChildren(options.controllers)
@@ -32,7 +35,18 @@ module.exports = class Resh {
     })
   }
 
-  start() {
+  onInit(cb) {
+    this.__onInit.push(cb)
+  }
+
+  async start() {
+    try {
+      await promiseAll(this.__onInit.map( cb => cb() ))
+    } catch(e) {
+      console.error('[Resh] 初始化时发生异常')
+      throw e
+    }
+
     this.router.makeSandwich()
     this.server.listen(...arguments)
   }
